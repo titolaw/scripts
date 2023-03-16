b775e40d700838c84170bc4959b042e6e1d6bdf5
titolaw <93658150+titolaw@users.noreply.github.com>
2023-03-16 10:20:40 -0300

    deleting comments

--- git.sh
+++ git.sh
@@ -7,28 +7,18 @@ current_time=$(date +%s%N)
 if [ "$1" == "obs" ]; then
   # Change to the ~/Documents/workspace_/receitas directory
   cd ~/Documents/workspace_/receitas
-
   # Add all changes in the current directory to the index
   git add .
-
   # Commit the changes with the current time as the commit message
   git commit -m "$current_time"
-
   # Push the changes to the remote repository
   git push
 elif [ "$1" == "dot" ]; then
   # Change to the ~/Documents/workspace_/receitas directory
   cd ~/.dotfiles
-
-  # Add all changes in the current directory to the index
   git add .
-
-  # Commit the changes with the current time as the commit message
   git commit -m "$current_time"
-
-  # Push the changes to the remote repository
   git push
-
 else
   echo "Invalid argument. Try "obs, dot""
 fi
